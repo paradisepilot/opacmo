65fa5fc2eb82160dda5d4424af97ae1d3be6f2de
Joachim Baran <joachim.baran@gmail.com>
2011-07-14 08:12:09 -0400

    Replaced median cut-off with a fixed cut-off value.

--- make_opacmo.sh
+++ make_opacmo.sh
@@ -3,13 +3,28 @@
 PATH=$PATH:./bioknack
 IFS=$(echo -e -n "\n\b")
 
-median() {
-	index=`cut -f 3 "$1" | sort -n -r | grep -v -E '1$' | wc -l`
-	let index=index/2
+# This is actually not used, but preserved here for future applications.
+#
+# Parameters:
+#  1. TSV filename. Third column of the TSV needs to hold the score.
+#  2. n
+#  3. m
+#
+#    Takes the value from the sorted (descreasing) list of scores at
+#    index: column_length * n / m
+#
+#    For median, use n = 1 and m = 2.
+#    For upper quartile, use n = 1 and m = 4.
+#    For lower quartile, use n = 3 and m = 4.
+#
+get_cut_value() {
+	index=`cut -f 3 "$1" | sort -n -r | grep -v -w '1' | wc -l`
+	let index=index*$2/$3
+	if [[ $index -lt 1 ]] ; then index=1 ; fi
 	return `cut -f 3 "$1" | sort -n -r | head -n $index | tail -n 1`
 }
 
-cut_below_median() {
+cut_below() {
 	sed -E 's/^.+-([0-9]+)	/\1	/' $2 \
 		| ruby -e "STDIN.each { |l|
 				l.chomp!
@@ -24,7 +39,7 @@ if [[ $# -lt 1 ]] || [[ $# -gt 2 ]] ; then
 	exit
 fi
 
-if [ "$1" != 'all' ] && [ "$1" != 'get' ] && [ "$1" != 'ner' ] ; then
+if [ "$1" != 'all' ] && [ "$1" != 'get' ] && [ "$1" != 'ner' ] && [ "$1" != 'tsv' ] ; then
 	echo "TODO: help message"
 	exit
 fi
@@ -66,20 +81,18 @@ if [ "$1" = 'all' ] || [ "$1" = 'ner' ] ; then
 fi
 
 if [ "$1" = 'all' ] || [ "$1" = 'tsv' ] ; then
+	echo "Joining genes, species and ontology terms..."
 	for genes in $data_dir/*__genes.tsv ; do
+		echo " - processing `basename "$genes" __genes.tsv`..."
 		species=$data_dir/`basename "$genes" __genes.tsv`__species.tsv
 		terms=$data_dir/`basename "$genes" __genes.tsv`__terms.tsv
 
 		tmp=`dirname "$genes"`
 		out=$data_dir/`basename "$genes" __genes.tsv`__joined.tsv
 
-		median $genes ; genes_median=$?
-		median $species ; species_median=$?
-		median $terms ; terms_median=$?
-
-		cut_below_median $genes_median $genes
-		cut_below_median $species_median $species
-		cut_below_median $terms_median $terms
+		cut_below 3 $genes
+		cut_below 3 $species
+		cut_below 3 $terms
 
 		join -t "	" -a 1 -a 2 -1 1 -2 1 -o 0,1.2,1.3,2.2,2.3 $genes.tmp $species.tmp > $tmp/genes_species.tmp
 		join -t "	" -a 1 -a 2 -1 1 -2 1 -o 0,1.2,1.3,1.4,1.5,2.2,2.3 $tmp/genes_species.tmp $terms.tmp > $out
