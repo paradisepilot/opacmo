887ad2d6d127c523cf5ebf0d176ed9e90e6f657b
Joachim Baran <joachim.baran@gmail.com>
2011-07-17 09:50:36 -0400

    Revamped UI.

--- html/mortar.js
+++ html/mortar.js
@@ -10,11 +10,90 @@
 
 var showHelpMessages = true;
 var showHelpMessagesElement = new Element('div#helpmessages', { 'class': 'headerswitch' });
+var aboutSlider = null;
+var aboutSwitch = new Element('div#aboutswitch');
+var caseSwitch = false;
+var caseSwitchElement = new Element('div#caseswitch');
 var helperSliders = {};
+
+var queryOverText = null;
+var suggestionSpinner = null;
+
+var workaroundHtmlTable = null;
+var workaroundHtmlTableRow = null;
 
 var suggestionTableCounter = 0;
 var suggestionTables = {};
 var suggestionColumns = {};
+
+var processQueryTimeOutID = 0;
+
+var column2Header = {
+		'pmcid':       'PMC ID',
+		'entrezname':  'Entrez Gene',
+		'entrezid':    'Entrez ID',
+		'speciesname': 'Species Name',
+		'speciesid':   'Species ID',
+		'oboname':     'OBO Term Name',
+		'oboid':       'OBO ID'
+	};
+
+var suggestionRequest = new Request.JSON({
+		url: 'http://opacmo.org/yoctogi.fcgi',
+		link: 'cancel',
+		onSuccess: function(response) {
+			suggestionSpinner.hide();
+
+			if (response.error) {
+				// TODO
+				alert(response['message']);
+				return;
+			}
+
+			clearSuggestions();
+			if (!response.result)
+				return;
+
+			for (var result in response.result) {
+				var partialResult = JSON.parse(response.result[result])['result']
+
+				if (partialResult.length == 0)
+					continue;
+
+				var id = makeTable($('suggestioncontainer'), partialResult, [column2Header[result]]);
+
+				suggestionColumns[id] = result;
+			}
+		}
+	});
+
+var resultRequest = new Request.JSON({
+		url: 'http://opacmo.org/yoctogi.fcgi',
+		link: 'cancel',
+		onSuccess: function(response) {
+			if (response.error) {
+				// TODO
+				alert(response);
+				return;
+			}
+
+			$('resultcontainer').empty();
+			makeTable($('resultcontainer'), response.result, [
+				'PMC ID',
+				'Entrez Gene',
+				'Entrez ID',
+				'Entrez Score',
+				'Species Name',
+				'Species ID',
+				'Species Score',
+				'OBO Term Name',
+				'OBO ID',
+				'OBO Score',
+				'PMC ID',
+				'PMC Title'
+			]);
+		}
+	});
 
 function clearSuggestions() {
 	var suggestions = $('suggestioncontainer').getChildren();
@@ -26,13 +105,20 @@ function clearSuggestions() {
 		var table = suggestionTables[suggestions[i].id];
 
 		if (!table || table.getSelected().length == 0) {
+			// Get rid of suggestions that were not selected.
 			delete suggestionTables[suggestions[i].id];
 			delete suggestionColumns[suggestions[i].id];
 			suggestions[i].dispose();
-		} else if ($('c' + suggestions[i].id).getOpacity() == 0)
-			new Fx.Morph($('c' + suggestions[i].id), { duration: 'long' }).start({
+		} else if ($('c' + suggestions[i].id).getOpacity() == 0) {
+			// Highlight those suggestions that were selected.
+			new Fx.Morph($('c' + suggestions[i].id), { duration: 'short' }).start({
 				opacity: [ 0, 1 ]
-			})
+			});
+			suggestionTables[suggestions[i].id].disableSelect();
+			suggestions[i].getChildren()[1].morph({
+				'color': '#bbbbbb'
+			});
+		}
 	}
 }
 
@@ -47,7 +133,8 @@ function makeTable(container, matrix, headers) {
 			cellspacing: 5
 		},
 		rows: matrix,
-		selectable: true
+		selectable: true,
+		allowMultiSelect: false
 	};
 
 	if (headers)
@@ -58,6 +145,7 @@ function makeTable(container, matrix, headers) {
 
 	var closeButton = new Element('img#c' + id, {
 		'class': 'closebutton',
+		title: 'bla',
 		src: '/images/gray_light/x_alt_12x12.png'
 	});
 	closeButton.addEvent('click', function() {
@@ -84,19 +172,23 @@ function makeTable(container, matrix, headers) {
 
 	var htmlTable = new HtmlTable(options);
 
-	htmlTable.addEvent('rowFocus', function() {
+	htmlTable.addEvent('rowFocus', function(row) {
+		if (workaroundHtmlTable && workaroundHtmlTable == htmlTable && workaroundHtmlTableRow == row)
+			htmlTable.deselectRow(row);
+
+		workaroundHtmlTable = null;
+
 		$('query').value = '';
 		runConjunctiveQuery();
 
 		if (showHelpMessages)
-			helperSliders['help1'].slideOut().chain(function() {
-				helperSliders['help1'].hide();
-				if (showHelpMessages)
-					helperSliders['help2'].slideIn();
-			});
+			helperSliders['help2'].slideIn();
 
 	});
-	htmlTable.addEvent('rowUnfocus', function() {
+	htmlTable.addEvent('rowUnfocus', function(row) {
+		workaroundHtmlTable = htmlTable;
+		workaroundHtmlTableRow = row;
+
 		runConjunctiveQuery();
 	});
 
@@ -117,53 +209,29 @@ function processQuery() {
 	if (!query || query.length == 0)
 		return;
 
-	if (showHelpMessages)
-		helperSliders['help0'].slideOut().chain(function() {
-			helperSliders['help0'].hide();
-			if (showHelpMessages)
-				helperSliders['help1'].slideIn();
-		});
+	query = query.replace(/^ +/, '')
+	query = query.replace(/ +$/, '')
+
+	if (showHelpMessages) {
+		helperSliders['help0'].slideIn();
+		helperSliders['help1'].slideIn();
+	}
 
 	var yoctogiClauses = {
 		pmcid: query,
 		entrezid: query,
-		gene: query,
-		taxid: query,
-		species: query,
-		doid: query,
-		disease: query,
-		goid: query,
-		goterm: query
+		entrezname: query,
+		speciesid: query,
+		speciesname: query,
+		oboid: query,
+		oboname: query
 	};
-	var yoctogiOptions = { like: true, batch: true }
+	var yoctogiOptions = { like: true, batch: true, distinct: true, caseinsensitive: !caseSwitch }
 
 	var yoctogiRequest = { clauses: yoctogiClauses, options: yoctogiOptions  }
 
-	new Request.JSON({
-		url: 'http://173.255.227.8/yoctogi.fcgi',
-		onSuccess: function(response) {
-			if (response.error) {
-				// TODO
-				alert(response);
-				return;
-			}
-
-			clearSuggestions();
-			if (!response.result)
-				return;
-
-			for (var result in response.result) {
-				var partialResult = JSON.parse(response.result[result])['result']
-
-				if (partialResult.length == 0)
-					continue;
-
-				var id = makeTable($('suggestioncontainer'), partialResult, [result]);
-
-				suggestionColumns[id] = result;
-			}
-		}
-	}).post(JSON.encode(yoctogiRequest));
+	suggestionSpinner.show();
+	suggestionRequest.send(JSON.encode(yoctogiRequest));
 }
 
 function runConjunctiveQuery() {
@@ -192,21 +260,9 @@ function runConjunctiveQuery() {
 		return;
 
 	var yoctogiOptions = {}
-	var yoctogiRequest = { clauses: yoctogiClauses, options: yoctogiOptions  }
+	var yoctogiRequest = { clauses: yoctogiClauses, dimensions: { titles: 'pmcid' }, options: yoctogiOptions  }
 
-	new Request.JSON({
-		url: 'http://173.255.227.8/yoctogi.fcgi',
-		onSuccess: function(response) {
-			if (response.error) {
-				// TODO
-				alert(response);
-				return;
-			}
-
-			$('resultcontainer').empty();
-			makeTable($('resultcontainer'), response.result)
-		}
-	}).post(JSON.encode(yoctogiRequest));
+	resultRequest.send(JSON.encode(yoctogiRequest));
 }
 
 function updateHelpMessagesSwitch() {
@@ -216,19 +272,66 @@ function updateHelpMessagesSwitch() {
 		$('helpmessages').innerHTML = 'Help Messages: Off';
 }
 
+function updateCaseSwitch() {
+	if (caseSwitch)
+		$('caseswitch').innerHTML = 'Case Sensitive Search: On&nbsp;';
+	else
+		$('caseswitch').innerHTML = 'Case Sensitive Search: Off';
+}
+
 $(window).onload = function() {
+	aboutSwitch.inject($('header'));
+	aboutSlider = new Fx.Slide('about', { mode: 'vertical', duration: 'short' }).hide();
+	aboutSlider.addEvent('complete', function() {
+		if (aboutSlider.open)
+			$('aboutswitch').innerHTML = 'Hide About';
+		else {
+			$('aboutswitch').innerHTML = 'Show About';
+			queryOverText.enable();
+		}
+	});
+	aboutSwitch.addEvent('click', function() {
+		$('about').style.visibility = 'visible';
+		queryOverText.disable();
+		aboutSlider.toggle();
+	});
+	$('aboutswitch').innerHTML = 'Show About';
+
 	showHelpMessagesElement.inject($('header'));
 	showHelpMessagesElement.addEvent('click', function() {
 		showHelpMessages = showHelpMessages ? false : true;
 		updateHelpMessagesSwitch();
+
+		if (showHelpMessages) {
+			helperSliders['help0'].slideIn();
+			helperSliders['help1'].slideIn();
+			helperSliders['help2'].slideIn();
+		} else {
+			helperSliders['help0'].slideOut();
+			helperSliders['help1'].slideOut();
+			helperSliders['help2'].slideOut();
+		}
 	});
 	updateHelpMessagesSwitch();
+
+	caseSwitchElement.inject($('querycontainer'));
+	caseSwitchElement.addEvent('click', function() {
+		caseSwitch = caseSwitch ? false : true;
+		updateCaseSwitch();
+		processQuery();
+	});
+	updateCaseSwitch();
 
 	helperSliders['help0'] = new Fx.Slide('help0', { mode: 'horizontal' }).hide().toggle();
 	helperSliders['help1'] = new Fx.Slide('help1', { mode: 'horizontal' }).hide();
 	helperSliders['help2'] = new Fx.Slide('help2', { mode: 'horizontal' }).hide();
 
-	$('query').addEvent('keyup', processQuery);
-	new OverText($('query'));
+	$('query').addEvent('keyup', function() {
+		clearTimeout(processQueryTimeOutID);
+		processQueryTimeOutID = processQuery.delay(250);
+	});
+	queryOverText = new OverText($('query'));
+
+	suggestionSpinner = new Spinner('suggestioncontainer');
 }
 
